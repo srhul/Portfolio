c950c1a9f49d94f94e5861c03cd9aab5bcc18d7d
Shanice <shanice.rhule@cthss.org>
2017-09-22 13:58:56 -0400

    Create menubar.js

---
+++ menubar.js
@@ -0,0 +1,6 @@
+document.write('\
+<ul class="topnav">\
+  <li><a class="active" href="https://srhul.github.io/Portfolio/home.html">Home</a></li>\
+  <li class="right"><a href="https://srhul.github.io/Portfolio/aboutauthor.htmll">Author</a></li>\
+</ul>\
+');
