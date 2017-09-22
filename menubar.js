a7dad08c90a2bc7079a645605cae873692d337ca
Shanice <shanice.rhule@cthss.org>
2017-09-22 15:06:04 -0400

    Delete menubar.js

--- menubar.js
+++
@@ -1,6 +0,0 @@
-document.write('\
-<ul class="topnav">\
-  <li><a class="active" href="https://srhul.github.io/Portfolio/home.html">Home</a></li>\
-  <li class="right"><a href="https://srhul.github.io/Portfolio/aboutauthor.htmll">Author</a></li>\
-</ul>\
-');
